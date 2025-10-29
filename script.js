34eb849df94fa0c3b9f2886c3075a147f1ffef48
ToptileGamer <jgautham42@gmail.com>
2025-10-30 00:48:17 +0530

    redesigned the entire web page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Handle registration button click
 document.getElementById("registerBtn").addEventListener("click", function() {
-  alert("Redirecting to Registration Page...");
+  // alert("Redirecting to Registration Page...");
   window.open("https://example.com/registration", "_blank");
 });
 
 // Handle map button click
 document.getElementById("mapBtn").addEventListener("click", function() {
   alert("Opening College Location...");
-  window.open("https://maps.google.com", "_blank");
+  window.open("https://maps.app.goo.gl/iodXiQ9fy5Sx1x9x7", "_blank");
 });
